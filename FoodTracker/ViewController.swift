72f4dc5be98edd8759aa0beb275f7fa1d66fa025
Thongphanh Duangboudda <thongphanhduangboudda@Thongphanhs-MacBook-Air.local>
2018-09-09 01:30:05 -0700

    try to push my branch

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,18 +8,70 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    //Mark: Properties
 
+    @IBOutlet weak var mealName: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var photoImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Loading App")
+    
+            textField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    // Mark: UItextfield delegations
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealName.text = textField.text
+        
+        
+    }
+    
+   
 
-
+    // Mark: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        
+        print("Clicked Button" )
+       mealName.text = textField.text
+        
+    }
+    
+    @IBAction func selectImageFromPhotoLibary(_ sender: UITapGestureRecognizer) {
+        
+        // Hide the keyboard
+        textField.resignFirstResponder()
+        
+        // UIImagePickerController is a view controller that lets a user pick media from photo libary
+        let imagePickerController = UIImagePickerController()
+        
+        // only allow photo to be pick, not taken.
+        imagePickerController.sourceType = .photoLibrary
+        
+        // make sure ViewController is notitied when the user picks an image.
+        
+        imagePickerController.delegate = self
+        
+        present(imagePickerController, animated: true, completion: nil)
+        
+        
+    }
 }
 
